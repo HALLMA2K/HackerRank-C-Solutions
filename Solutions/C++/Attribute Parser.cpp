201173fd45acb410f1d6caf3a7bdf31251d47a98
HALLMA2K <80502136+HALLMA2K@users.noreply.github.com>
2023-01-08 15:08:30 -0800

    Update Attribute Parser.cpp

--- Solutions/C++/Attribute Parser.cpp
+++ Solutions/C++/Attribute Parser.cpp
@@ -2,71 +2,90 @@
 
 using namespace std;
 
-int main(){
-    int n, q,i;
-    cin >> n >> q;
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int numLines, numQueries, i;
+    cin >> numLines >> numQueries;
 
     string temp;
-    vector<string> hrml;
-    vector<string> query;
+    // vector to store the lines of HTML code
+    vector<string> htmlLines;
+    // vector to store the queries
+    vector<string> queries;
 
     cin.ignore();
 
-    for(i = 0; i < n; i++) {
-        getline(cin,temp);
-        hrml.push_back(temp);
+    for (i = 0; i < numLines; i++) {
+        getline(cin, temp);
+        htmlLines.push_back(temp);
     }
-    
-    for(i = 0; i < q; i++) {
-        getline(cin,temp);
-        query.push_back(temp);
+
+    for (i = 0; i < numQueries; i++) {
+        getline(cin, temp);
+        queries.push_back(temp);
     }
-    
-    map<string, string> m;
-    vector<string> tag;
-    
-    for(i = 0; i < n; i++) {
-        temp = hrml[i];
-        temp.erase(remove(temp.begin(), temp.end(), '\"' ),temp.end());
-        temp.erase(remove(temp.begin(), temp.end(), '>' ),temp.end());
-        
-        if(temp.substr(0, 2) == "</") {
-            tag.pop_back();
+
+    // map to store the parsed HTML tags and their attributes
+    map<string, string> attributeMap;
+    // vector to store the tag names
+    vector<string> tagNames;
+
+    for (i = 0; i < numLines; i++) {
+        temp = htmlLines[i];
+        // remove double quotes from the line
+        temp.erase(remove(temp.begin(), temp.end(), '\"'), temp.end());
+        // remove angle brackets from the line
+        temp.erase(remove(temp.begin(), temp.end(), '>'), temp.end());
+
+        if (temp.substr(0, 2) == "</") {
+            // closing tag, remove the last tag from the vector
+            tagNames.pop_back();
         }
-        else{
+        else {
             stringstream ss;
             ss.str("");
             ss << temp;
-            
-            string t1, p1, v1;
+
+            string tag, prop, val;
             char ch;
-            ss >> ch >> t1 >> p1 >> ch >> v1;
-            
-            string temp1 = "";
-            if(tag.size() > 0) {
-                temp1 =* tag.rbegin();
-                temp1 = temp1 + "." + t1;
+            // parse the line and extract the tag name, property name and property value
+            ss >> ch >> tag >> prop >> ch >> val;
+
+            string tagWithParent = "";
+            if (tagNames.size() > 0) {
+                // this is not the root tag, prepend the parent tag's name
+                tagWithParent = *tagNames.rbegin();
+                tagWithParent = tagWithParent + "." + tag;
             }
-            else{
-                temp1 = t1;
+            else {
+                tagWithParent = tag;
             }
-            
-            tag.push_back(temp1);
-            m[*tag.rbegin() + "~" + p1] = v1;
-            
-            while(ss){
-                ss >> p1 >> ch >> v1;
-                m[*tag.rbegin() + "~" + p1] = v1;
+
+            tagNames.push_back(tagWithParent);
+            // store the attribute in the map
+            attributeMap[tagWithParent + "~" + prop] = val;
+
+            while (ss) {
+                // extract additional properties from the line
+                ss >> prop >> ch >> val;
+                attributeMap[tagWithParent + "~" + prop] = val;
             }
         }
     }
-    for(i = 0; i < q; i++){
-        if (m.find(query[i]) == m.end()) {
+
+    for (i = 0; i < numQueries; i++) {
+        // find the attribute with the name specified in the query
+        if (attributeMap.find(queries[i]) == attributeMap.end()) {
             cout << "Not Found!\n";
         }
-        else{
-            cout << m[query[i]] << endl;
+        else {
+            // print the value of the attribute
+            cout << attributeMap[queries[i]] << endl;
         }
     }
+
     return 0;
 }
