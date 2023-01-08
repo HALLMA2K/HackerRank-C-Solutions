0535412c8bb96958c9914c2c1371acbdc58f722d
HALLMA2K <80502136+HALLMA2K@users.noreply.github.com>
2023-01-07 23:59:14 -0800

    Create Basic Data Types.cpp

---
+++ Solutions/C++/Basic Data Types.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    double c, d;
+    char e;
+    
+    scanf("%d %ld %c %lf %lf", &a, &b, &e, &c, &d);
+    printf("%d\n%ld\n%c\n%f\n%lf", a, b, e, c, d);
+    
+    return 0;
+}
